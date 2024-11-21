f0e766fa077e044ce824f63a5a490ae9c84ddd32
Akshay sharma <akshaysharmaak57@gmail.com>
2024-11-21 23:10:16 +0530

    add project  files

---
+++ sctipt.js
@@ -0,0 +1,47 @@
+let box1=document.querySelector(".box1");
+let box2=document.querySelector(".box2");
+let box3=document.querySelector(".box3");
+let box4=document.querySelector(".box4");
+let box5=document.querySelector(".box5");
+let box6=document.querySelector(".box6");
+
+let hamico=document.querySelector("#hamicon");
+
+hamico.onclick=()=>{
+    console.log("hamberger\n");
+}
+
+
+box1.onclick=()=>{
+    window.location.href=("desktop/index.html");
+};
+
+box2.onclick=()=>{
+    window.location.href=("laptop/index.html");
+};
+
+box3.onclick=()=>{
+    window.location.href=("console/index.html");
+};
+
+box4.onclick=()=>{
+    window.location.href=("mobil/index.html");
+};
+
+box5.onclick=()=>{
+    window.location.href=("power supply and adapters/index.html");
+};
+
+box6.onclick=()=>{
+    window.location.href=("other electronics/index.html");
+};
+
+
+
+// let box1=head.children[0];
+   
+
+//  box1.innerText="fuck you butcer";
+//  box1.style.opacity=1;
+
+
